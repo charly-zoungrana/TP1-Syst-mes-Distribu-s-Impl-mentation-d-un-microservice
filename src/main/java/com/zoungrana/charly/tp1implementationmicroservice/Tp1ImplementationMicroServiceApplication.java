3d2a1105f94a51c5866648bfa47464198cc6e388
ZOUNGRANA Charly <z.charly@peris-solutions.com>
2026-09-21 22:59:11 +0000

    First Commit

---
+++ src/main/java/com/zoungrana/charly/tp1implementationmicroservice/Tp1ImplementationMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zoungrana.charly.tp1implementationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1ImplementationMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1ImplementationMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoungrana/charly/tp1implementationmicroservice/Tp1ImplementationMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoungrana.charly.tp1implementationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1ImplementationMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
